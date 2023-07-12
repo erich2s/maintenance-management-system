a679028b0a0a1365144f23c343c2d21d7e2b0e10
eric <tarot1754443943@gmail.com>
2023-07-12 18:01:37 +0800

    hot fix

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-// export async function GET(req: Request,res:Response) {
-//     const users = await prisma.user.findMany();
-//     const session = await getServerSession(authOptions)
-//     console.log(session);
-//     const token = await getToken({ req, secret: process.env.SECRET });
-//     console.log(token);
-//     return NextResponse.json(users);
-//   }
-  
