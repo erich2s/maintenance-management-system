103d52f83bde3346751adbd3bdb1f00d8d24e4ad
eric <tarot1754443943@gmail.com>
2023-08-11 14:20:56 +0800

    修复默认分页的情况

--- src/app/api/locations/route.ts
+++ src/app/api/locations/route.ts
@@ -5,13 +5,19 @@ export async function GET(req: NextRequest) {
   // 分页，每页10条
   const { searchParams } = new URL(req.url);
   const page = (Number(searchParams.get("page")) || 1) - 1;
-  const size = Number(searchParams.get("size")) || 10;
-  const data = await prisma.location.findMany({
-    skip: page * size,
-    take: size,
-  });
-  const total = await prisma.location.count();
-  return NextResponse.json({ data, total });
+  const size = Number(searchParams.get("size"));
+  if (size) {
+    const data = await prisma.location.findMany({
+      skip: page * size,
+      take: size,
+    });
+    const total = await prisma.location.count();
+    return NextResponse.json({ data, total });
+  } else {
+    // 不分页
+    const data = await prisma.location.findMany();
+    return NextResponse.json({ data });
+  }
 }
 
 export async function POST(req: NextRequest) {
--- src/app/api/types/route.ts
+++ src/app/api/types/route.ts
@@ -5,13 +5,20 @@ export async function GET(req: NextRequest) {
   // 分页，每页10条
   const { searchParams } = new URL(req.url);
   const page = (Number(searchParams.get("page")) || 1) - 1;
-  const size = Number(searchParams.get("size")) || 10;
-  const data = await prisma.type.findMany({
-    skip: page * size,
-    take: size,
-  });
-  const total = await prisma.type.count();
-  return NextResponse.json({ data, total });
+  const size = Number(searchParams.get("size"));
+  if (size) {
+    const data = await prisma.type.findMany({
+      skip: page * size,
+      take: size,
+    });
+    const total = await prisma.type.count();
+    return NextResponse.json({ data, total });
+  } else {
+    // 不分页
+    console.log("不分页");
+    const data = await prisma.type.findMany();
+    return NextResponse.json({ data });
+  }
 }
 
 export async function POST(req: NextRequest) {
